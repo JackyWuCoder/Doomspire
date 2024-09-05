591753ad0499e62b6ff8ad840806a04d26a25789
JackyWuCoder <JackyWuCoder@gmail.com>
2024-09-05 13:48:52 -0700

    Basic Shooting Mechanic

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private float bulletSpeed = 50.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Left mouse button to shoot
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.velocity = bulletSpawnPoint.forward * bulletSpeed;
+    }
+}
